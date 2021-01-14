98fbc9df6d0074c21aa7669d5f032295d8f0bd42
Aleksandra Kowalczyk <55331922+demensiss@users.noreply.github.com>
2021-01-14 12:09:29 +0100

    add new file to project

---
+++ distance-calculator.py
@@ -0,0 +1,9 @@
+# 8 of 10 application - Winter Holidays Challenge 2021
+
+# Napisz program liczący odległość liniową między dwoma dowolnymi punktami na mapie, wykorzystujący ich
+# współrzędne geograficzne (długość i szerokość geograficzną). Wykorzystaj dowolny algorytm,
+# np. https://pl.wikibooks.org/.../Astrono.../Odleg%C5%82o%C5%9Bci
+# Skorzystaj z API (np. https://rapidapi.com/trueway/api/trueway-geocoding), żeby obliczyć odległość
+# pomiędzy twoim adresem, a charakterystycznymi punktami np. Wieżą Eiffla czy Tadź Mahal.
+#
+# Propozycja rozszerzenia: zamiast podawać swój adres, użyj geolokalizacji
