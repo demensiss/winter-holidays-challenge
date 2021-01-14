e423247f44251f0e60bc2a47d6ad3925fe502f5c
Aleksandra Kowalczyk <55331922+demensiss@users.noreply.github.com>
2021-01-14 12:05:49 +0100

    opening the photo folder

--- image-resizer.py
+++ image-resizer.py
@@ -7,3 +7,16 @@
 
 # Propozycja rozszerzenia: Oblicz ile miejsca na dysku można oszczędzić po kompresji 
 # (odczytaj rozmiar plików w pierwotnym folderze oraz "smaller" i porównaj obie wartości - bezwzględnie i w %)
+
+from PIL import Image
+import glob
+import os
+
+folder = "winter-holidays-challenge/images"
+os.chdir(folder)
+
+new_folder = "smaller"
+
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile)
