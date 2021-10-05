ef3792bf9d0da3f8831910c7264acd606f135b16
Aleksandra Kowalczyk <55331922+demensiss@users.noreply.github.com>
2021-10-05 21:19:58 +0200

    create a password generating program

--- password-generator.py
+++ password-generator.py
@@ -5,3 +5,27 @@
 # Warto skorzystać z modułów string i secrets.
 
 # Propozycja rozszerzenia: Po wygenerowaniu hasła skopiuj je do schowka systemowego
+
+import random
+import string
+from pyperclip import copy
+
+LETTERS = string.ascii_letters
+NUMS = string.digits
+SPECJAL = string.punctuation
+POSSIBLE_SYMBOLS = LETTERS + NUMS + SPECJAL
+
+while True:
+    try:
+        len_password = int(input("Enter password length: "))
+        if len_password < 8:
+            raise Exception("Your password is too short. Try again.")
+        break
+    except Exception as e:
+        print(e)
+
+password = ''.join(random.sample(POSSIBLE_SYMBOLS, len_password))
+
+print(f"Your generated passoword is: {password}")
+copy(password)
+
